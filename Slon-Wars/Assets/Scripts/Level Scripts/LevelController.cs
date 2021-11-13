8c1ac671f25bab6849890ff0820efe4bf87c8481
adam-steven <55250489+adam-steven@users.noreply.github.com>
2021-11-13 19:11:42 +0000

    Revert "Level Generation"
    This reverts commit 2554b9fd97e4319266510c0a0dc030f98759f772.

--- Slon-Wars/Assets/Scripts/Level Scripts/LevelController.cs
+++ Slon-Wars/Assets/Scripts/Level Scripts/LevelController.cs
@@ -4,12 +4,49 @@ using UnityEngine;
 
 public class LevelController : MonoBehaviour
 {
+
+    public int maxRooms;
+    [HideInInspector]
+    public int targetRooms;
+
+    private int[,] grid;
+    public int gridSize;
+
+    public GameObject room;
     // Start is called before the first frame update
     void Start()
     {
+        //make sure the grid is odd so that there is always a center
+        gridSize = (gridSize % 2 == 0) ? gridSize - 1 : gridSize;
+        grid = new int[gridSize, gridSize];
         
+        CreateLevel();
     }
 
+    private void printGrid()
+    {
+        string tableValue = "";
+        for (int i = 0; i < grid.GetLength(0); i++)
+        {
+            string rowValues = "";
+            for (int j = 0; j < grid.GetLength(1); j++)
+            {
+                rowValues += grid[i, j].ToString() + " ";
+            }
+            tableValue += rowValues + "\n";
+        }
+        Debug.Log(tableValue);
+    }
+
+    private void CreateLevel()
+    {
+        targetRooms = Random.Range(Mathf.FloorToInt(maxRooms / 2), maxRooms);
+        int centerOfGrid = Mathf.CeilToInt(gridSize / 2f);
+        grid[centerOfGrid, centerOfGrid] = 1;
+
+        printGrid();
+        //GameObject startingRoom = Instantiate(room, transform.position, transform.rotation);
+    }
     public void CheckEnemies()
     {
         if (GameObject.FindGameObjectWithTag("Enemy"))
--- Slon-Wars/Assets/Scripts/Level Scripts/Room.cs
+++ Slon-Wars/Assets/Scripts/Level Scripts/Room.cs
@@ -4,40 +4,15 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
-    private string doorCode;
-    private bool isBoss;
-    private bool isStart;
-    private bool enemiesSpawend;
-
-    public GameObject wallWithDoor;
-    public GameObject wall;
-
-    public void InitiliseValue(string doorCode, bool isBoss, bool isStart)
+    // Start is called before the first frame update
+    void Start()
     {
-        this.doorCode = doorCode;
-        this.isBoss = isBoss;
-        this.isStart = isStart;
-        this.enemiesSpawend = isStart;
-
-        CreateWalls();
-
-        if(isBoss && isStart)
-        {
-            Debug.Log("ONE ROOM FLOOR");
-        }
+        
     }
 
-    private void CreateWalls()
+    // Update is called once per frame
+    void Update()
     {
-        string[] directions = { "Up", "Down", "Left", "Right" };
-
-        for (int i = 0; i < directions.Length; i++)
-        {
-            Transform currentWallPosition = this.transform.Find(directions[i]).gameObject.transform;
-            GameObject currentWallPrefab = (doorCode.Contains(directions[i].Substring(0, 1))) ? wallWithDoor : wall;
-
-            GameObject spawnedWall = Instantiate(currentWallPrefab, currentWallPosition.position, currentWallPosition.rotation);
-            spawnedWall.transform.parent = currentWallPosition.transform;
-        }
+        
     }
 }
